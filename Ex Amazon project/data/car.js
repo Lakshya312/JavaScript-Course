a6f9955522ae734f36d98b210813b549650fd0a4
Lakshya Pal <pallakshya077@gmail.com>
2025-07-12 23:08:17 +0530

    Ex 17b

--- Ex Amazon project/data/car.js
+++ Ex Amazon project/data/car.js
@@ -6,10 +6,14 @@ export class Car{
     this.brand = brand;
     this.model = model;
   }
+
+  displayInfo(){
+    console.log(`${this.brand} ${this.model}`);
+  }
 }
 
 const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 
-console.log(car1);
-console.log(car2);
+car1.displayInfo();
+car2.displayInfo();
