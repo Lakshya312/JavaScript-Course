41a5dd4416665be092284d2f8813595fbeb53af0
Lakshya Pal <pallakshya077@gmail.com>
2025-07-13 12:40:55 +0530

    Ex 17c

--- Ex Amazon project/data/car.js
+++ Ex Amazon project/data/car.js
@@ -1,6 +1,7 @@
 export class Car{
   brand;
   model;
+  speed = 0;
 
   constructor(brand, model){
     this.brand = brand;
@@ -8,12 +9,33 @@ export class Car{
   }
 
   displayInfo(){
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+  }
+
+  go(){
+    if(this.speed<=200){
+    this.speed+=5;
+    }
+  }
+
+  brake(){
+    if(this.speed>=0){
+      this.speed-=5;
+    }  
   }
 }
 
 const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 
+car1.displayInfo();
+car2.displayInfo();
+car1.go();
+car1.go();
+car2.go();
+car1.displayInfo();
+car2.displayInfo();
+car1.brake();
+car2.brake();
 car1.displayInfo();
 car2.displayInfo();
