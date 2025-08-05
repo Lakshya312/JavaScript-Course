229331f322b2233574de0bb131ecd1723d1b30db
Lakshya Pal <pallakshya077@gmail.com>
2025-08-05 16:41:19 +0530

    Ex 18n

--- Ex Amazon project/scripts/orders.js
+++ Ex Amazon project/scripts/orders.js
@@ -1,6 +1,2 @@
-import { cart } from "../data/cart-class.js";
-import { orders } from "../data/orders.js";
 import { renderOrderPage} from "./checkout/orderPageSummary.js";
 renderOrderPage();
-console.log(orders)
-console.log(cart.cartItems);
---
+++ Ex Amazon project/scripts/tracking.js
@@ -0,0 +1,67 @@
+import { getOrder } from "../data/orders.js";
+import {getProduct, loadProductsFetch} from '../data/products.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+trackingPageSummary();
+
+async function trackingPageSummary(){
+   await loadProductsFetch();
+ 
+   const url = new URL(window.location.href)
+   const orderId = url.searchParams.get('orderId');
+   const productId = url.searchParams.get('productId');
+  
+   const order = getOrder(orderId);
+   const product = getProduct(productId);
+
+   let productDetails ;
+
+   order.products.forEach(details => {
+      if(details.productId === productId){
+         productDetails = details;
+      }
+   });
+   
+   const orderStringTiming = dayjs(productDetails.estimatedDeliveryTime).format('MMMM D');
+   
+   let trackPageHTML = '';
+   
+   trackPageHTML += `<div class="order-tracking">
+           <a class="back-to-orders-link link-primary" href="orders.html">
+             View all orders
+           </a>
+   
+           <div class="delivery-date">
+             Arriving on ${orderStringTiming}
+           </div>
+   
+           <div class="product-info">
+             ${product.name}
+           </div>
+   
+           <div class="product-info">
+             Quantity: ${productDetails.quantity}
+           </div>
+   
+           <img class="product-image" src="${product.image}">
+   
+           <div class="progress-labels-container">
+             <div class="progress-label">
+               Preparing
+             </div>
+             <div class="progress-label current-status">
+               Shipped
+             </div>
+             <div class="progress-label">
+               Delivered
+             </div>
+           </div>
+   
+           <div class="progress-bar-container">
+             <div class="progress-bar"></div>
+           </div>
+         </div>`;
+       
+   document.querySelector('.js-main')
+       .innerHTML = trackPageHTML;
+}
